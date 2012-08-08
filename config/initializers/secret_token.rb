a40dec1d67745428acb5fc4acae0db04b4303ac6
Arnaud Benard <arnaudbenard13@gmail.com>
2012-08-07 21:38:52 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opentok::Application.config.secret_token = '2fdf28e6359e23e6388877145e0a335aa5d5e2374949409f6090d10a7c431b7237d5d7d2db3460d5dd356fba54dc4f8416560edd8984bf07a4c9284c16e6a248'
